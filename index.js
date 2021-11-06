c74bc6ac3a41045c1dd8164445c2b79544985ea0
Bryan Xu <xubryan123@gmail.com>
2021-11-06 10:37:55 -0700

    added check authentication functionality

--- index.js
+++ index.js
@@ -37,21 +37,21 @@ app.use(passport.session());
 
 // Routes start here
 
-app.get("/reminders", reminderController.list);
+app.get("/reminders", ensureAuthenticated, reminderController.list);
 
-app.get("/reminder/new", reminderController.new);
+app.get("/reminder/new", ensureAuthenticated,reminderController.new);
 
-app.get("/reminder/:id", reminderController.listOne);
+app.get("/reminder/:id", ensureAuthenticated,reminderController.listOne);
 
 app.get("/reminder/:id/edit", ensureAuthenticated, reminderController.edit);
 
-app.post("/reminder/", reminderController.create);
+app.post("/reminder/", ensureAuthenticated,reminderController.create);
 
 // Implement this yourself
-app.post("/reminder/update/:id", reminderController.update);
+app.post("/reminder/update/:id", ensureAuthenticated,reminderController.update);
 
 // Implement this yourself
-app.post("/reminder/delete/:id", reminderController.delete);
+app.post("/reminder/delete/:id", ensureAuthenticated,reminderController.delete);
 
 // Fix this to work with passport! The registration does not need to work, you can use the fake database for this.
 app.get("/register", forwardAuthenticated, authController.register);
